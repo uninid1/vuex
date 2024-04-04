5fdb8ef36a328e1b6f6df6cc3144239f3091de85
uninid1 <160439845+uninid1@users.noreply.github.com>
2024-04-04 16:37:43 +1000

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1 :style="titleStyle">Пример приложения с использованием Vuex</h1>
+    <p>Счетчик: {{ $store.state.count }}</p>
+    <button @click="increment" :style="buttonStyle">Увеличить</button>
+    <button @click="decrement" :style="buttonStyle">Уменьшить</button>
+    <button @click="reset" :style="resetButtonStyle">Сбросить</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      buttonStyle: {
+        backgroundColor: '#4CAF50',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        marginRight: '10px'
+      },
+      titleStyle: {
+        color: '#009688',
+      },
+      resetButtonStyle: {
+        backgroundColor: '#f44336',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+      }
+    };
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment');
+    },
+    decrement() {
+      this.$store.commit('decrement');
+    },
+    reset() {
+      this.$store.commit('reset');
+    }
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    decrement(state) {
+      state.count--
+    },
+    reset(state) {
+      state.count = 0
+    }
+  }
+});
